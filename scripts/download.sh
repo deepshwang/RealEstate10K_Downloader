70788038b5b92e11beabb1f3055734732ebef4d8
Sungwon Hwang <shwang.14@kaist.ac.kr>
2025-02-06 15:55:32 +0900

    minor changes

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,4 +2,5 @@ num_splits=${1:-8}
 split_idx=${2:-0}
 num_threads=${3:-8}
 
+python generate_dataset.py train --split_idx $split_idx --num_splits $num_splits --num_threads $num_threads
 python generate_dataset.py test --split_idx $split_idx --num_splits $num_splits --num_threads $num_threads
